45e3af13fba60937801a559a4ee2e3dceadb8ef6
sunneepark <seonhuibag1228@gmail.com>
2019-11-06 01:26:34 +0900

    modify naverlogin

--- routes/naver/login.js
+++ routes/naver/login.js
@@ -12,8 +12,12 @@ var api_url = "";
 router.get('/naverlogin', function (req, res) {
   console.log("sdc");
   api_url = 'https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=' + client_id + '&redirect_uri=' + redirectURI + '&state=' + state;
-   res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
-   res.end("<a href='"+ api_url + "'><img height='50' src='http://static.nid.naver.com/oauth/small_g_in.PNG'/></a>");
+  res.status(201).send({
+    message : "success",
+    api_url : api_url
+  });
+  //res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
+   //res.end("<a href='"+ api_url + "'><img height='50' src='http://static.nid.naver.com/oauth/small_g_in.PNG'/></a>");
  });
  router.get('/callback', function (req, res) {
     code = req.query.code;
