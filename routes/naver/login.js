c1f84bf03a8d1b03ea2bc6f697629ae0a7035220
sunneepark <seonhuibag1228@gmail.com>
2019-11-06 03:58:19 +0900

    modify login

--- routes/naver/login.js
+++ routes/naver/login.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app=express();
 var router = express.Router();
+var request = require('request');
 
 var client_id = 'n5L6cIX3kIJmf3CoR0hW';
 var client_secret = 'Nd43kUpV7M';
@@ -10,17 +11,14 @@ var redirectURI = encodeURI("https://"+url+"/naver/callback");
 var api_url = "";
 
 router.get('/naverlogin', function (req, res) {
-  console.log("sdc");
+
   api_url = 'https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=' + client_id + '&redirect_uri=' + redirectURI + '&state=' + state;
-  //res.status(201).send({
-  //  message : "success",
-  //  api_url : api_url
-  //});
-  res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
-  res.end("<a href='"+ api_url + "'><img height='50' src='http://static.nid.naver.com/oauth/small_g_in.PNG'/></a>");
+   res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
+   res.end("<a href='"+ api_url + "'><img height='50' src='http://static.nid.naver.com/oauth/small_g_in.PNG'/></a>");
  });
+
  router.get('/callback', function (req, res) {
-    code = req.query.code;
+     code = req.query.code;
     state = req.query.state;
     api_url = 'https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id='
      + client_id + '&client_secret=' + client_secret + '&redirect_uri=' + redirectURI + '&code=' + code + '&state=' + state;
@@ -28,40 +26,33 @@ router.get('/naverlogin', function (req, res) {
     var options = {
         url: api_url,
         headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
-    }
+     };
     request.get(options, function (error, response, body) {
       if (!error && response.statusCode == 200) {
-        //res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
-        //res.end(body);
+        var request = require('request');
         let result=JSON.parse(body);
         
-        res.status(201).send({
-          message : 'success',
-          name : result.access_token
+        var options = {
+            url: "https://"+url+"/naver/member/"+result.access_token
+        };
+        request.get(options, function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+            res.end("로그인 성공하였습니다.");
+          }else{
+            res.status(response.statusCode).end();
+            console.log('error = ' + response.statusCode);
+          }
         });
-        // var option={
-        //     url: 'https://'+url+'/naver/member',
-        //     headers: {'access_token':result.access_token}    
-        // }
-        // request.get(option,function(error,response,body){
-        //     if(!error && response.statusCode == 201){
-              
-        //       res.end(body);
-              
-
-        //     }else{
-        //         res.end("internal server error");
-        //     }
-        // });
       } else {
         res.status(response.statusCode).end();
         console.log('error = ' + response.statusCode);
       }
     });
- });
- router.get('/member', function (req, res) {
+});
+
+ router.get('/member/:token', function (req, res) {
   
-    var header="Bearer "+req.headers.access_token;
+    var header="Bearer "+req.params.token;
     console.log(header);
     var api_url = 'https://openapi.naver.com/v1/nid/me';
     var request = require('request');
@@ -73,11 +64,11 @@ router.get('/naverlogin', function (req, res) {
       if (!error && response.statusCode == 200) {
           
         var result=JSON.parse(body);
-        console.log(result.response.name);
-        res.status(201).send({
-            message : 'success',
-            name : result.response.email
+        console.log(result.response.email);
+        res.status(200).send({
+            message : 'success'
         });
+
         return;
       } else {
         console.log('error');
