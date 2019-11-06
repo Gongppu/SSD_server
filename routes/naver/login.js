7001ce0a598757a49c1536c2e72eb0600ccd55a8
sunneepark <seonhuibag1228@gmail.com>
2019-11-06 10:44:14 +0900

    login screen modified

--- routes/naver/login.js
+++ routes/naver/login.js
@@ -3,8 +3,8 @@ var app=express();
 var router = express.Router();
 var request = require('request');
 
-var client_id = 'n5L6cIX3kIJmf3CoR0hW';
-var client_secret = 'Nd43kUpV7M';
+var client_id = 'ztKxe6pwd1G6_AC5aWtA';
+var client_secret = 'PVMF5Q5Put';
 var state = "RAMDOM_STATE";
 var url="sharesdocument.ml"
 var redirectURI = encodeURI("https://"+url+"/naver/callback");
@@ -14,7 +14,72 @@ router.get('/naverlogin', function (req, res) {
 
   api_url = 'https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=' + client_id + '&redirect_uri=' + redirectURI + '&state=' + state;
    res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
-   res.end("<a href='"+ api_url + "'><img height='50' src='http://static.nid.naver.com/oauth/small_g_in.PNG'/></a>");
+   var str = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <style>
+        .vertical_center {
+            position:absolute;
+            top:50%; left:50%;
+            transform: translate(-50%, -50%);
+        }
+
+        .horizontal_center{
+            text-align: center;
+        }
+        .login_title{
+            font-size: 26px;
+            font-weight: bold;
+            font-stretch: normal;
+            font-style: normal;
+            line-height: 1.19;
+            letter-spacing: normal;
+        }
+
+        .login_detail{
+            margin-top: 20px;
+            font-size: 14px;
+            font-weight: 300;
+            font-stretch: normal;
+            font-style: normal;
+            line-height: 1.21;
+            letter-spacing: normal;
+            color: #818181;
+        }
+        .login_logo_div{
+          margin-top:20px;
+        }
+        .login_logo{
+            width: 100px;
+            height: 100px;
+            border: solid 1px orange;
+        }
+        .login_button{
+          margin-top:20px;
+        }
+    </style>
+</head>
+<body>
+<div class="vertical_center">
+    <div class="horizontal_center">
+        <div class="login_title">
+            <span style="color: orange">S</span>hare
+            <span style="color: orange">S</span>crap
+            <span style="color: orange">D</span>ocument
+        </div>
+
+        <div class="login_detail">
+            쓷 - 스크랩한 내용을 <br>
+            문서로 작성하고 공유하세요!
+        </div>
+        <div class=login_logo_div>
+        <img class=login_logo src= 'https://ssdfilebucket.s3.ap-northeast-2.amazonaws.com/KakaoTalk_20191106_051613493.png'>
+        </div>
+       <a href= \"`+api_url+ `\"><img class=login_button height='50' src='http://static.nid.naver.com/oauth/small_g_in.PNG'/></a>
+    `;
+   res.end(str);
  });
 
  router.get('/callback', function (req, res) {
@@ -31,18 +96,20 @@ router.get('/naverlogin', function (req, res) {
       if (!error && response.statusCode == 200) {
         var request = require('request');
         let result=JSON.parse(body);
-        
+
         var options = {
             url: "https://"+url+"/naver/member/"+result.access_token
         };
+console.log(options);
         request.get(options, function (error, response, body) {
           if (!error && response.statusCode == 200) {
-            res.end("로그인 성공하였습니다.");
+            res.end("success login");
           }else{
             res.status(response.statusCode).end();
             console.log('error = ' + response.statusCode);
           }
         });
+        return;
       } else {
         res.status(response.statusCode).end();
         console.log('error = ' + response.statusCode);
@@ -51,7 +118,7 @@ router.get('/naverlogin', function (req, res) {
 });
 
  router.get('/member/:token', function (req, res) {
-  
+
     var header="Bearer "+req.params.token;
     console.log(header);
     var api_url = 'https://openapi.naver.com/v1/nid/me';
@@ -62,14 +129,11 @@ router.get('/naverlogin', function (req, res) {
      };
     request.get(options, function (error, response, body) {
       if (!error && response.statusCode == 200) {
-          
+
         var result=JSON.parse(body);
         console.log(result.response.email);
-        res.status(200).send({
-            message : 'success'
-        });
-
         return;
+
       } else {
         console.log('error');
         if(response != null) {
@@ -78,5 +142,6 @@ router.get('/naverlogin', function (req, res) {
         }
       }
     });
+        res.status(200).end();
   });
  module.exports = router;
