422ae982cf4f7be537cecf7d0d89de4e5e306ace
sunneepark <seonhuibag1228@gmail.com>
2019-11-10 12:14:16 +0900

    파일올리기 추가

---
+++ routes/doc/file.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+const upload = require('../../config/multer.js');
+
+router.post('/',upload.single('board_img'), async function(req, res){
+    
+    //파일올리기
+    if(!req.file){
+        res.status(400).send({
+            message : "Null Value"
+        }); 
+    }else {
+        board_img = req.file.location;
+        res.status(200).send({
+            message : "success",
+            location : board_img
+        }); 
+    }
+});
+module.exports = router;
--- routes/doc/index.js
+++ routes/doc/index.js
@@ -3,15 +3,20 @@ const router = express.Router();
 
 
 
-//doc read
-router.use('/', require('./doc.js'));
-
-
 //doc add
 router.use('/add', require('./add.js'));
 
 //doc list get
 router.use('/list', require('./list.js'));
+
+//doc file add
+router.use('/file', require('./file.js'));
+
+//doc read
+router.use('/', require('./doc.js'));
+
+
+
 //doc get
 //router.use('/get', require('./doc_get.js'));
 
